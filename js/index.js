43b64a7098370bbc45a340ff8abfe66f035fb319
GeorgeHWC <georgechan223@gmail.com>
2020-04-02 00:47:09 +0100

    save work

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 function background() {
 
     document.getElementById("main").classList.add("mainindex");
-
+    setTimeout(typeWriter, 2500);
+    // typeWriter();
+    console.log("js is working");
 }
 
 function showMenu() {
@@ -14,6 +16,32 @@ function showMenu() {
     }
 
 }
+
+var i = 0;
+var txt = ' This page is create 100% by my self manually!'; /* The text */
+var speed = 70; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+
+    if (i < txt.length) {
+        document.getElementById("typing").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+var i = 1; //  set your counter to 1
+
+function myLoop() { //  create a loop function
+    setTimeout(function() { //  call a 3s setTimeout when the loop is called
+        alert('hello'); //  your code here
+        i++; //  increment the counter
+        if (i < 10) { //  if the counter < 10, call the loop function
+            myLoop(); //  ..  again which will trigger another 
+        } //  ..  setTimeout()
+    }, 3000)
+}
+
 
 // const menuToggle = document.getElementById("menu-toggle");
 // const menuNav = document.getElementById("menu-nav");
